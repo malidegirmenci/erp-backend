9aceb9eb729daa00c1190914cbb432e915c56211
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2025-05-11 19:05:49 +0300

    listens ports

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // PORT ortam değişkenini dinle, yoksa 3000'i kullan
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
